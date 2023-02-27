f60557fefdd0dfefd1eff369d126151b019d4f43
Augusto Etz <augustoetz@hotmail.com>
2023-02-27 16:31:11 -0300

    feat: creating Monster

---
+++ src/Monster.ts
@@ -0,0 +1,25 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number; 
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints() { return this._lifePoints; }
+  get strength() { return this._strength; }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const hp = this._lifePoints - attackPoints;
+    if (hp <= 0) {
+      this._lifePoints = -1;
+    } else { this._lifePoints = hp; }
+    return this._lifePoints;
+  }
+}
